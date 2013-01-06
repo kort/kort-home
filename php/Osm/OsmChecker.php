5ae15613ecd3d751b507096e6fc860827cb2d1c0
Stefan Oderbolz <oderbolz@gmail.com>
2013-01-06 23:52:30 +0100

    Fixed linting errors

--- php/Osm/OsmChecker.php
+++ php/Osm/OsmChecker.php
@@ -1,11 +1,24 @@
 <?php
+/**
+ * kort - Osm\OsmChecker class
+ */
 
 namespace Osm;
 
 use Helper\HttpHelper;
 
-class OsmChecker {
-
+/**
+ * The OsmChecker class is used to check information from OpenStreetMap.
+ */
+class OsmChecker
+{
+    /**
+     * Return true if the supplied $fix is not yet in OpenStreetMap.
+     *
+     * @param array $fix The information of fix from Kort.
+     *
+     * @return boolean True if the fix is not yet in OpenStreetMap, false otherwise
+     */
     public static function notChangedInOsm(array $fix)
     {
         $http = new HttpHelper();
