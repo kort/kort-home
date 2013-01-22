809a09f0e4a2c61060835448a0bda8a389f6f01a
Juerg Hunziker <juerg.hunziker@gmail.com>
2013-01-22 07:36:22 +0100

    fixed behaviour when osm object wasn't found

--- php/Osm/OsmChecker.php
+++ php/Osm/OsmChecker.php
@@ -25,16 +25,20 @@ class OsmChecker
         $osmWsUrl = "http://play.kort.ch/server/webservices/osm/" . $fix['osm_type']. "/" . $fix['osm_id'];
         $result = $http->get($osmWsUrl);
 
-        $doc = new \DOMDocument();
-        $doc->loadXML($result);
-        $xPath = new \DOMXpath($doc);
-
-        $queryResult = $xPath->query("//node()[@id=" . $fix['osm_id'] . "]/tag[@k='".$fix['osm_tag']."']");
-
         $value = null;
-        $element = $queryResult->item(0);
-        if (!empty($element)) {
-            $value = $element->getAttribute('v');
+        if(!empty($result)) {
+            $doc = new \DOMDocument();
+            $doc->loadXML($result);
+            $xPath = new \DOMXpath($doc);
+
+            $queryResult = $xPath->query("//node()[@id=" . $fix['osm_id'] . "]/tag[@k='".$fix['osm_tag']."']");
+
+            $element = $queryResult->item(0);
+            if (!empty($element)) {
+                $value = $element->getAttribute('v');
+            }
+        } else {
+            $value = "OSM Object not found!";
         }
         return empty($value);
     }
