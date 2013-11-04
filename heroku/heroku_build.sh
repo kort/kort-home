1e76cfae17a24251cd7c2d69aedec9bdb5613fc4
Juerg Hunziker <juerg.hunziker@gmail.com>
2013-11-04 22:01:10 +0100

    added itg presentation

--- heroku/heroku_build.sh
+++ heroku/heroku_build.sh
@@ -16,6 +16,7 @@ cp -r $SOURCE_DIR/gispunkt $BUILD_DIR
 cp -r $SOURCE_DIR/idw $BUILD_DIR
 cp -r $SOURCE_DIR/fossgis $BUILD_DIR
 cp -r $SOURCE_DIR/swisscom $BUILD_DIR
+cp -r $SOURCE_DIR/itg $BUILD_DIR
 cp -r $SOURCE_DIR/proposals $BUILD_DIR
 cp -r $SOURCE_DIR/*.php $BUILD_DIR
 cp -r $SOURCE_DIR/*.html $BUILD_DIR
