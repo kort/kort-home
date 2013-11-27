5430a80abe73837f0926032916303afb62cec0fd
Juerg Hunziker <juerg.hunziker@gmail.com>
2013-11-27 18:27:38 +0100

    created idw presentation

---
+++ webteam/index.php
@@ -0,0 +1,9 @@
+<?php
+if($_SERVER['HTTP_HOST'] == 'localhost') {
+    $redirectUrl = 'http://localhost/kort-home';
+} else {
+    $redirectUrl = 'http://www.kort.ch';
+}
+$redirectUrl .= '/presentation/webteam';
+
+header('Location: ' . $redirectUrl);
