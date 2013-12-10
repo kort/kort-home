7781a769a3e5dfd45f272f401c6b7bc4ebc69b10
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2013-12-10 09:55:22 +0100

    Make sure PRs do not trigger a heroku deployment

--- heroku/heroku.sh
+++ heroku/heroku.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
-if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
-   echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
-   exit 1
+if [[ $TRAVIS_SECURE_ENV_VARS == "false" ]] ; then
+    DEPLOY="false"
 fi
 
-export SOURCE_DIR=$CI_HOME
+echo "=> on branch $TRAVIS_BRANCH"
+if [[ $TRAVIS_BRANCH != "master" ]] ; then
+    DEPLOY="false"
+fi
 
-yes | ruby $CI_HOME/heroku/heroku_prepare.rb
-bash $CI_HOME/heroku/heroku_keys.sh
-bash $CI_HOME/heroku/heroku_build.sh
-cd $BUILD_DIR
-bash $CI_HOME/heroku/heroku_add.sh >/dev/null
-bash $CI_HOME/heroku/heroku_push.sh
+
+if [[ $DEPLOY == "true" ]] ; then
+    if [ -z $BUILD_DIR -a -z $CI_HOME ] ; then
+       echo "You need to specify the BUILD_DIR and CI_HOME environment variable."
+       exit 1
+    fi
+
+    export SOURCE_DIR=$CI_HOME
+
+    yes | ruby $CI_HOME/heroku/heroku_prepare.rb
+    bash $CI_HOME/heroku/heroku_keys.sh
+    bash $CI_HOME/heroku/heroku_build.sh
+    cd $BUILD_DIR
+    bash $CI_HOME/heroku/heroku_add.sh >/dev/null
+    bash $CI_HOME/heroku/heroku_push.sh
+else
+    echo "Omitting deployment to heroku"
+fi
